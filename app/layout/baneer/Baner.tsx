918fae8fb0934999a16a8942733b502e0eb6af6f
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-02-16 14:49:39 +0600

    Carousel

--- app/layout/baneer/Baner.tsx
+++ app/layout/baneer/Baner.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { FC } from 'react'
+import Image from 'next/image'
 import styles from './Baner.module.scss'
+import baner from '../../../public/baner1.png'
+import { Carousel } from 'react-responsive-carousel'
 
-const Baner = () => {
-	return <div>Baner</div>
+const Baner: FC = () => {
+	return (
+		<div className={styles.slider_wrapper}>
+			<Carousel
+				autoPlay
+				infiniteLoop
+				showStatus={false}
+				showIndicators={false}
+				showThumbs={false}
+				interval={5000}
+			>
+				<div>
+					<Image loading='lazy' className={styles.slider} src={baner} alt='' />
+					<p className={styles.slider_title}>Legend 1</p>
+				</div>
+				<div>
+					<Image loading='lazy' className={styles.slider} src={baner} alt='' />
+					<p className={styles.slider_title}>Legend 2</p>
+				</div>
+				<div>
+					<Image loading='lazy' className={styles.slider} src={baner} alt='' />
+					<p className={styles.slider_title}>Legend 3</p>
+				</div>
+			</Carousel>
+		</div>
+	)
 }
 
 export default Baner
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from '@/app/layout/header/Header'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import NavBar from '@/app/layout/navbar/NavBar'
+import Baner from '@/app/layout/baneer/Baner'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
 			<main>
 				<Header />
 				<NavBar />
+				<Baner />
 			</main>
 		</>
 	)
