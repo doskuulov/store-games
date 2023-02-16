621dc4444fefe046b24d36eb3497f068f27c3d61
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-02-16 15:18:58 +0600

    upgrade slider

--- app/layout/baneer/Baner.tsx
+++ app/layout/baneer/Baner.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react'
 import Image from 'next/image'
 import styles from './Baner.module.scss'
-import baner from '../../../public/baner1.png'
+import baner1 from '../../../public/baner1.png'
+import baner2 from '../../../public/baner4.jpg'
+import baner3 from '../../../public/baner3.jpg'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
 import { Carousel } from 'react-responsive-carousel'
 
 const Baner: FC = () => {
@@ -16,15 +19,30 @@ const Baner: FC = () => {
 				interval={5000}
 			>
 				<div>
-					<Image loading='lazy' className={styles.slider} src={baner} alt='' />
+					<Image
+						loading='lazy'
+						className={styles.slider}
+						src={baner1}
+						alt='baner'
+					/>
 					<p className={styles.slider_title}>Legend 1</p>
 				</div>
 				<div>
-					<Image loading='lazy' className={styles.slider} src={baner} alt='' />
+					<Image
+						loading='lazy'
+						className={styles.slider}
+						src={baner2}
+						alt='baner'
+					/>
 					<p className={styles.slider_title}>Legend 2</p>
 				</div>
 				<div>
-					<Image loading='lazy' className={styles.slider} src={baner} alt='' />
+					<Image
+						loading='lazy'
+						className={styles.slider}
+						src={baner3}
+						alt='baner'
+					/>
 					<p className={styles.slider_title}>Legend 3</p>
 				</div>
 			</Carousel>
