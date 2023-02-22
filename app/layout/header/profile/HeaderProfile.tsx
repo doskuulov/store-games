4d70bc777e7fbfb2d4d017166ada7b65f0d7553b
Doskulov Mirbek <iskender.doskulov@gmail.com>
2023-02-22 19:24:51 +0600

    adding cart

--- app/layout/header/profile/HeaderProfile.tsx
+++ app/layout/header/profile/HeaderProfile.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react'
 import Image from 'next/image'
+import { FiUser } from 'react-icons/fi'
 import profile from '../../../../public/profile.png'
+import Vector from '../../../../public/Vector.png'
+import Union from '../../../../public/Union.png'
 import styles from './HeaderProfile.module.scss'
 
 const HeaderProfile: FC = () => {
@@ -10,6 +13,21 @@ const HeaderProfile: FC = () => {
 				<div className={styles.profile__name}>Doskulov Ernis</div>
 			</div>
 			<Image src={profile} alt='profile' className={styles.profile__img} />
+			{/* media block */}
+			<div className={styles.card}>
+				<Image
+					width={22}
+					src={Union}
+					alt='profile'
+					className={styles.card__img}
+				/>
+				<Image
+					width={22}
+					src={Vector}
+					alt='profile'
+					className={styles.card__img}
+				/>
+			</div>
 		</div>
 	)
 }
